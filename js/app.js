f9589b5ca30569e56e46b67d598c5fba4d60186c
nemaaalqetami <nemaa-alqetami@hotmail.com>
2021-03-17 01:20:52 +0200

    Adding Voting to Text

--- js/app.js
+++ js/app.js
@@ -18,14 +18,13 @@ let ext =['jpg','jpg','jpg','jpg',
 // 'dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg',
 // 'scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg',
 // 'unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg',];
-let arr =[];
-
+let votes =[];
 let nameProduct = [];
+let allViews = [];
 
 const main = document.getElementById('main');
 let button = document.createElement('button');
 button.setAttribute('class','button');
-
 let UnorderList = document.createElement('ul');
 let item ;
 const section = document.getElementById('productSection');
@@ -38,10 +37,11 @@ function Product(name,imgExt)
 {
     this.name = name;
     this.views = 0;
-    this.count =25; 
+    this.count =0; 
     this.path = `./assets/${name}.${imgExt}`;
     this.votes =0;
     Product.all.push(this);
+    
 }
 Product.all=[];
 Product.lastShown=[];
@@ -49,65 +49,83 @@ for (let i=0;i<img.length;i++){
     new Product(img[i],ext[i]);
     
 }
+  
+
+
 
 function render(){
 
-    
-
-
-   let firstIndex = randomNumber(0,Product.all.length-1) ;
-    let secondtIndex = randomNumber(0,Product.all.length-1) ;
+ let firstIndex = randomNumber(0,Product.all.length-1) ;
+    let secondIndex = randomNumber(0,Product.all.length-1) ;
     let thirdIndex = randomNumber(0,Product.all.length-1) ;
 
-while(firstIndex===secondtIndex||firstIndex===thirdIndex||secondtIndex===thirdIndex
-    ||  Product.lastShown.includes(firstIndex)|| Product.lastShown.includes(secondtIndex)|| Product.lastShown.includes(thirdIndex) ){
+while(firstIndex===secondIndex||firstIndex===thirdIndex||secondIndex===thirdIndex
+    ||  Product.lastShown.includes(firstIndex)|| Product.lastShown.includes(secondIndex)|| Product.lastShown.includes(thirdIndex) ){
        firstIndex = randomNumber(0,Product.all.length-1) ;
-       secondtIndex = randomNumber(0,Product.all.length-1) ;
+       secondIndex = randomNumber(0,Product.all.length-1) ;
        thirdIndex = randomNumber(0,Product.all.length-1) ;
        }
 
        
+  
     
+
     const firstRandomProduct = Product.all[firstIndex];
     firstImage.src= firstRandomProduct.path; 
      firstImage.title = firstRandomProduct.name;
      firstImage.alt = firstRandomProduct.name;
      Product.lastShown[0]=firstIndex;
+     firstRandomProduct.views+= 1;
+  
+   
+     console.log( Product.all[firstIndex].name+Product.all[firstIndex].views);
 
-    
-    const secondRandomProduct = Product.all[secondtIndex];
+    const secondRandomProduct = Product.all[secondIndex];
        secondtImage.src = secondRandomProduct.path; 
       secondtImage.title = secondRandomProduct.name;
       secondtImage.alt = secondRandomProduct.name;
-     Product.lastShown[1]= secondtIndex;
-    
+     Product.lastShown[1]= secondIndex;
+     secondRandomProduct.views+= 1;
+
+ 
+ console.log( Product.all[secondIndex].name +Product.all[secondIndex].views);
+
+
     const thirdRandomProduct= Product.all[thirdIndex];
     thirdImage.src = thirdRandomProduct.path;
     thirdImage.alt = thirdRandomProduct.name;
     thirdImage.title = thirdRandomProduct.name;
      Product.lastShown[2]=thirdIndex;
+     thirdRandomProduct.views+= 1;
+       
 
+  
+     console.log(  Product.all[thirdIndex].name+Product.all[thirdIndex].views);
+
+ 
+
+   
 }
 
 section.addEventListener('click',getData);
     
     function getData(event){
         if(event.target.id!=='productSection'){
-        
+       
         for(let i = 0; i<Product.all.length;i++){
-             Product.all[i].votes++;
+            Product.all[i].count++;
             
             if(Product.all[i].name === event.target.title){
         
-          Product.all[i].views++;  
-          
-       
-             arr.push(Product.all[i].views);
+          Product.all[i].votes++;  
+         
+   
+             votes.push(Product.all[i].votes);
           
             nameProduct.push(event.target.title);
-
+            
           
-             if(Product.all[i].count === Product.all[i].votes){ 
+             if(Product.all[i].count === 25){ 
                  section.removeEventListener('click',getData);
                  button.classList.add('btn');
                  button.textContent='View Result';
@@ -115,11 +133,15 @@ section.addEventListener('click',getData);
                  main.appendChild(button);
                
                  document.querySelector('.btn').addEventListener('click',getItem) ;
+
+                 for(let i = 0;i<Product.all.length;i++){
+                   allViews.push(Product.all[i].views);
+                 }
                 createChart();
 
 
           }
-         console.log(event.target.title+" "+Product.all[i].views);
+      
       
         }
        
@@ -148,11 +170,11 @@ function randomNumber(min,max){
 function getItem(){
  
                           
-    for(let i = 0; i <25 ; i++){
-    
+    for(let i = 0; i <allViews.length; i++){
+   
         item = document.createElement('li');
-  
-        item.innerHTML = nameProduct [i]+ " has " + arr[i]+ " views";  
+ 
+        item.innerText = nameProduct [i]+ " has " + votes[i]+ " Votes, " + allViews[i] + " views";  
         UnorderList.appendChild(item);
         item.setAttribute('class','li');
  
@@ -167,26 +189,32 @@ main.appendChild(UnorderList)
         let context = document.getElementById('myChart').getContext('2d');
     
       let name = [];
-      let views =[];
+      let votes =[];
+    
         for(let i=0;i<Product.all.length;i++){
             name.push(Product.all[i].name);
 
-           views.push(Product.all[i].views);
-           
+           votes.push(Product.all[i].votes);
+         
         }
       
         let chartObject={
          
-          type: 'bar',
+          type: 'horizontalBar',
         
           data: {
               labels:name,
               datasets: [{
-                  label: 'Products views results',
+                  label: 'Products votes results',
                   backgroundColor: 'rgb(0, 138, 138)',
                   
-                  data: views,
+                  data: votes
               },
+              {
+                label : 'Products views results',
+                backgroundColor: 'rgb(37, 214, 214)',
+                data : allViews
+              }
 
             ]
         
